7f7375e326b497da9107ff29845287bf08b02fbb
emmakyber <emma.c.kyber@vanderbilt.edu>
2024-02-17 17:53:40 +0100

    maybe this time

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 const gulp = require('gulp');
 const sass = require('./build/sass');
 const scripts = require('./build/scripts');
-const images = require('./build/images');
+const images = require('./build/images')(gulp);
 const sync = require('./build/browsersync');
 
 [sass, scripts, images, sync].forEach(task => {
